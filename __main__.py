4aa534691eb90958e9f8ef24f5ad1270b6b41c4c
Adil780 <fortito831@gmail.com>
2020-11-30 12:05:30 +0100

    1 version

--- __main__.py
+++ __main__.py
@@ -1,2 +1,12 @@
-import pygame
+import pygame, time
+from pygame import display as dis, event
 pygame.init()
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+
+screen = dis.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+while True:
+    time.sleep(1)
+    t = event.get()
